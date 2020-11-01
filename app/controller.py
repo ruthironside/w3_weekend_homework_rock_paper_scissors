abcacb7469b5710f456d24b0375a72aa4932fcb9
Ruth Ironside <ruthironside@live.com>
2020-11-01 14:10:50 +0000

    add some structure to html

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.models.players import players
 from app.models.player import *
@@ -8,18 +8,18 @@ from app.models.game import *
 def index():
     return render_template('index.html')
 
-# @app.route('/<choice>') 
-# def play(choice): 
-#     return f"Player 1 wins by playing {choice}!"
+@app.route('/<player1_choice>/<player2_choice>') 
+def play(player1_choice, player2_choice): 
+    return play_game(player1, player2)
 
 @app.route('/play-game', methods=['POST', 'GET'])
 def play_game():
-    player1_name = request.form['player1_name']
-    player1_choice = request.form['player1_choice']
+    player1_name = request.form('player1_name')
+    player1_choice = request.form('player1_choice')
     player1 = Player(name=player1_name, choice=player1_choice)
 
-    player2_name = request.form['player2_name']
-    player2_choice = request.form['player2_choice']
+    player2_name = request.form('player2_name')
+    player2_choice = request.form('player2_choice')
     player2 = Player(name=player2_name, choice=player2_choice)
 
     game1 = Game(player1, player2)
