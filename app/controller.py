16a7be0746b87d3b3143666e589f35d4505cf402
Ruth Ironside <ruthironside@live.com>
2020-11-01 09:15:22 +0000

    added app route for play game

--- app/controller.py
+++ app/controller.py
@@ -2,11 +2,30 @@ from flask import render_template
 from app import app
 from app.models.players import players
 from app.models.player import *
+from app.models.game import *
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home', players=players)
+    return render_template('index.html')
 
-@app.route('/<choice>') 
-def play(choice): 
-    return f"Player 1 wins by playing {choice}!"
+# @app.route('/<choice>') 
+# def play(choice): 
+#     return f"Player 1 wins by playing {choice}!"
+
+@app.route('/play-game', methods=['POST', 'GET'])
+def play_game():
+    player1_name = request.form['player1_name']
+    player1_choice = request.form['player1_choice']
+    player1 = Player(name=player1_name, choice=player1_choice)
+
+    player2_name = request.form['player2_name']
+    player2_choice = request.form['player2_choice']
+    player2 = Player(name=player2_name, choice=player2_choice)
+
+    game1 = Game(player1, player2)
+    result = game1.play_game(player1_choice, player2_choice)  
+
+    return render_template('index.html', result=result)
+    # return redirect('/')
+
+
